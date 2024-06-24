51398572844362e65c33193ad2d48ec880e24553
TzuruC <tzuruc@gmail.com>
2024-06-25 03:42:30 +0800

    input practice

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,34 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="">
+  <div>{{ name }}</div>
+  <label>v-model 鍵入即得 </label>
+  <input type="text" v-model="name" /><br />
+  <hr />
+  <label>v-model.trim 去除頭尾空白 </label>
+  <input type="text" v-model.trim="name" />
+  <hr />
+  <label>v-model.lazy 游標跳出 input 輸入框才會寫入變數 </label>
+  <input type="text" v-model.lazy="name" />
+  <hr />
+
+  <label>v-once 防止顯示的變數被更改(不明) </label>
+  <p v-once>{{ name }}</p>
+  <input type="text" v-model="name" />
+  <hr />
+  <label>v-pre 直接印出標籤內的原字</label>
+  <p v-pre>{{ name }}</p>
+  <input type="text" v-model.lazy="name" />
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      go: "go",
+      name: "",
+    };
+  },
+};
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
